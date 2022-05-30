b469fb214dd3e2f1e74ad981221dd2b2f6e7f8b7
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-05-31 00:10:47 +0600

    navigration setup

--- App.js
+++ App.js
@@ -1,58 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import {
-    StyleSheet,
-    Text,
-    View,
-    ImageBackground,
-    TouchableOpacity,
-} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import Home from "./Screens/HomeScreen";
+import StepCounter from "./Screens/StepCounter";
+
+const Stack = createNativeStackNavigator();
 export default function App() {
     return (
-        <View style={styles.container}>
-            <ImageBackground
-                style={{ flex: 1 }}
-                resizeMode="cover"
-                source={require("./assets/woman-2592247_1920.jpg")}
-            >
-                <View style={styles.btnFather}>
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.startBtn}>Let's Start</Text>
-                    </TouchableOpacity>
-                </View>
-            </ImageBackground>
-            <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Step" component={StepCounter} />
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-    },
-    btnFather: {
-        flex: 1,
-        alignItems: "flex-start",
-        marginLeft: 30,
-        marginBottom: 150,
-
-        justifyContent: "flex-end",
-    },
-    button: {
-        alignItems: "center",
-        justifyContent: "center",
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: "blue",
-    },
-    startBtn: {
-        fontSize: 16,
-        lineHeight: 21,
-        fontWeight: "bold",
-        letterSpacing: 0.25,
-        color: "white",
-    },
-});
---
+++ Screens/HomeScreen.js
@@ -0,0 +1,63 @@
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Text,
+    View,
+    ImageBackground,
+    TouchableOpacity,
+} from "react-native";
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View>
+            <View style={styles.container}>
+                <ImageBackground
+                    style={{ flex: 1 }}
+                    resizeMode="cover"
+                    source={require("../assets/woman-2592247_1920.jpg")}
+                >
+                    <View style={styles.btnFather}>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate("Step")}
+                            style={styles.button}
+                        >
+                            <Text style={styles.startBtn}>Let's Start</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ImageBackground>
+                <StatusBar style="auto" />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    btnFather: {
+        flex: 1,
+        alignItems: "flex-start",
+        marginLeft: 30,
+        marginBottom: 150,
+
+        justifyContent: "flex-end",
+    },
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: "blue",
+    },
+    startBtn: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: "bold",
+        letterSpacing: 0.25,
+        color: "white",
+    },
+});
+export default HomeScreen;
