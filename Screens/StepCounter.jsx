73d140059368605b0bb1f9207c77c44a98dad106
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-05-30 22:52:39 +0600

    home screen backgruound Image

---
+++ Screens/StepCounter.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const StepCounter = () => {
+    return <View></View>;
+};
+
+export default StepCounter;
+
+const styles = StyleSheet.create({});
